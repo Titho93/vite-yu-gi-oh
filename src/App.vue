6e4fc7859e4d7a85cf7d44ab8317347a39ca6a2c
titho93 <titho93.10@gmail.com>
2023-10-12 14:56:53 +0200

    [add]structure

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script>
 export default {
-
+  name: 'App'
 }
 </script>
 
 <template>
+  <h1>Yu-Gi-Oh Api</h1>
   
 </template>
 
-<style>
+<style lang="scss">
+
+@use './scss/Main.scss';
 
 </style>
